c1256a3a9a60f2a24352c647a1bf0ab854beb5a4
bru16b <brunitollanos@gmail.com>
2026-03-24 23:21:58 -0300

    Funcionalidad4

--- questions.py
+++ questions.py
@@ -1,52 +1,54 @@
 import random
-words = {
+dic = {
     "colores" : ["azul", "rojo", "verde", "violeta", "amarillo", "negro"],
     "deportes" : ["futbol", "handball", "tenis", "ajedrez", "natacion"],
     "paises" : ["Argentina", "Peru", "Bolivia", "Francia", "Alemania", "Bulgaria"]
 }
-guessed = []
-attempts = 6
+
 print("¡Bienvenido al Ahorcado!")
 print()
 print(f"Categorias disponibles: ")
-for elem in words:
+for elem in dic:
     print(elem)
 
 categoria = input("Ingrese con que categoria jugar: ")
-while categoria not in words:
+while categoria not in dic:
     categoria = input("Categoria invalida ingrese otra: ")
-word = random.choice(words[categoria])
+words = random.sample(dic[categoria], len(dic[categoria]))
 print()
 
-while attempts > 0:
+for word in words:   #vamos a jugar con todas las palabras de la categoria elegida
+    guessed = []  
+    attempts = 6
+    while attempts > 0:
 # Mostrar progreso: letras adivinadas y guiones para las que faltan
-    progress = ""
-    for letter in word:
-        if letter in guessed:
-            progress += letter + " "
-        else:
-            progress += "_ "
-    print(progress)
+        progress = ""
+        for letter in word:
+            if letter in guessed:
+                progress += letter + " "
+            else:
+                progress += "_ "
+        print(progress)
 # Verificar si el jugador ya adivinó la palabra completa
-    if "_" not in progress:
-        print("¡Ganaste!")
-        break
-    print(f"Intentos restantes: {attempts}")
-    print(f"Letras usadas: {', '.join(guessed)}") #Concatena elementos en un string con , 
-    letter = input("Ingresá una letra: ")
-    if len(letter) == 1 and letter.isalpha():
-        if letter in guessed:
-            print("Ya usaste esa letra.")
-        elif letter in word:
-            guessed.append(letter)
-            print("¡Bien! Esa letra está en la palabra.")
+        if "_" not in progress:
+            print("¡Ganaste!")
+            break
+        print(f"Intentos restantes: {attempts}")
+        print(f"Letras usadas: {', '.join(guessed)}") #Concatena elementos en un string con , 
+        letter = input("Ingresá una letra: ")
+        if len(letter) == 1 and letter.isalpha():
+            if letter in guessed:
+                print("Ya usaste esa letra.")
+            elif letter in word:
+                guessed.append(letter)
+                print("¡Bien! Esa letra está en la palabra.")
+            else:
+                guessed.append(letter)
+                attempts -= 1
+                print("Esa letra no está en la palabra.")
+            print()
         else:
-            guessed.append(letter)
-            attempts -= 1
-            print("Esa letra no está en la palabra.")
-        print()
+            print("Entrada no valida")
     else:
-        print("Entrada no valida")
-else:
-    print(f"¡Perdiste! La palabra era: {word}")
-print(f"Puntaje final: {attempts}") #Los intentos podrian servir como puntaje, ya que arranca en 6 y por cada error resta uno.
+        print(f"¡Perdiste! La palabra era: {word}")
+    print(f"Puntaje final: {attempts}") #Los intentos podrian servir como puntaje de ronda, ya que arranca en 6 y por cada error resta uno.
